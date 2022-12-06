5c797ce5fa1de45c022e5fb0c4dc4d20b8d52c83
Tomas Michalek <tmichalek@sygic.com>
2022-12-06 23:50:54 +0100

    feat: Solution for Day 02, Part 2

--- 03_Rucksack_Reorganization/src/rr/core.clj
+++ 03_Rucksack_Reorganization/src/rr/core.clj
@@ -22,11 +22,23 @@
     backpacks
     ))
 
+(defn
+  total-elf-groups-value
+  "Gets total value of elves grouped by group-size"
+  [backpacks group-size]
+  (map
+    #(sum (items-value (apply common-items %)))
+    (partition group-size backpacks)
+    ))
+
 (defn -main
   [& args]
   (let [lines (get-lines (first args))]
     (println
       "Common items total value"
       (sum (total-common-items-value lines)))
+    (println
+      "Elf groups total value"
+      (sum (total-elf-groups-value lines 3)))
     )
   )
