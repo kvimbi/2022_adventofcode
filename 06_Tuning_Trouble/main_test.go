46171dab43a99b61658498c7083dcfd251d85107
Tomas Michalek <tmichalek@sygic.com>
2022-12-07 21:17:20 +0100

    feat: Solution Day 06, Part 2

--- 06_Tuning_Trouble/main_test.go
+++ 06_Tuning_Trouble/main_test.go
@@ -2,23 +2,37 @@ package main
 
 import "testing"
 
-func TestString1(t *testing.T) {
-	result := FindStartPacket("bvwbjplbgvbhsrlpgdmjqwftvncz")
+func TestString1Marker(t *testing.T) {
+	result := FindStartPacket("bvwbjplbgvbhsrlpgdmjqwftvncz", 4)
 	if result != 5 {
 		t.Fail()
 	}
 }
 
 func TestString2(t *testing.T) {
-	result := FindStartPacket("nppdvjthqldpwncqszvftbrmjlhg")
+	result := FindStartPacket("nppdvjthqldpwncqszvftbrmjlhg", 4)
 	if result != 6 {
 		t.Fail()
 	}
 }
 
 func TestString3(t *testing.T) {
-	result := FindStartPacket("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+	result := FindStartPacket("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4)
 	if result != 10 {
+		t.Fail()
+	}
+}
+
+func TestString1Message(t *testing.T) {
+	result := FindStartPacket("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14)
+	if result != 19 {
+		t.Fail()
+	}
+}
+
+func TestString2Message(t *testing.T) {
+	result := FindStartPacket("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14)
+	if result != 29 {
 		t.Fail()
 	}
 }
